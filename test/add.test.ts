0d86fb97e5403fd42e07e31450e9a3d8ddc350c3
Teepan <paartheepan94@gmail.com>
2024-10-26 16:40:32 +0200

    feat: add add function

---
+++ test/add.test.ts
@@ -0,0 +1,17 @@
+import {describe, expect, it} from 'vitest'
+import {add} from '../src/calculator/add'
+
+type TestCase = {numbers: [number, number], expected: number};
+describe('additions', () => {
+  it
+    .each<TestCase>([
+      {numbers: [1,1], expected: 2},
+      {numbers: [1,2], expected: 3},
+      {numbers: [1,3], expected: 4},
+      {numbers: [1,4], expected: 5},
+      {numbers: [1,5], expected: 6},
+    ])
+    ('should add numbers', ({numbers, expected}) => {
+      expect(add(...numbers)).toEqual(expected)
+    })
+})
